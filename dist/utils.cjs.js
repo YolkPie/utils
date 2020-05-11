c699ac9f2e1c3f2562013500041980075d601548
yujihu <yujihu@jd.com>
2020-05-11 10:33:41 +0800

    feat(rem.js): 支持配置设计稿宽度

--- dist/utils.cjs.js
+++ dist/utils.cjs.js
@@ -1354,15 +1354,17 @@ function jsonpAdapter (config) {
 var rem = ((doc, win) => {
   const docEl = doc.documentElement;
   const resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
-  function recalc () {
-    const designWidth = 750;
+  function recalc (designWidth) {
+    designWidth = designWidth || 750;
     const clientWidth = docEl.clientWidth || window.screen.width;
     if (!clientWidth) return
     docEl.style.fontSize = (100 * clientWidth) / designWidth + 'px';
   }
-  return function () {
-    recalc();
-    win.addEventListener && win.addEventListener(resizeEvt, recalc, false);
+  return function (designWidth) {
+    recalc(designWidth);
+    win.addEventListener && win.addEventListener(resizeEvt, function () {
+      recalc(designWidth);
+    }, false);
   }
 })(document, window);
 
